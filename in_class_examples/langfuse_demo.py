f84ac37bf6e32a70333d1bf39c7b5445cce4b0db
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-19 13:27:43 -0400

    added in class examples

---
+++ in_class_examples/langfuse_demo.py
@@ -0,0 +1,28 @@
+from langfuse.decorators import observe
+from langfuse.openai import openai # OpenAI integration
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGFUSE_SECRET_KEY"] = os.getenv("LANGFUSE_SECRET_KEY")
+os.environ["LANGFUSE_PUBLIC_KEY"] = os.getenv("LANGFUSE_PUBLIC_KEY")
+os.environ["LANGFUSE_HOST"] = os.getenv("LANGFUSE_HOST")
+ 
+@observe()
+def story():
+    return openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=100,
+        messages=[
+          {"role": "system", "content": "You are a great storyteller."},
+          {"role": "user", "content": "Once upon a time in a galaxy far, far away..."}
+        ],
+    ).choices[0].message.content
+ 
+@observe()
+def main():
+    return story()
+ 
+main()
